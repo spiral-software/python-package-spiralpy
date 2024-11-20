ca2f7c0229cb6028c07419e22e267ee1aec0c0ed
Mike Franusich <mike.franusich@gmail.com>
2024-11-20 17:16:33 -0500

    change formerly deprecated np.float to float

--- spiralpy/hockneysolver.py
+++ spiralpy/hockneysolver.py
@@ -171,7 +171,7 @@ class HockneySolver(SPSolver):
         ns = self._problem.dimNS()
 		
         ret = np.array([[[(i*ns**2+1)+(j*ns)+(k) for i in range(ns)]for j in range(ns)] \
-			for k in range(ns)]).astype(np.float)
+			for k in range(ns)]).astype(float)
 
         return ret
      
